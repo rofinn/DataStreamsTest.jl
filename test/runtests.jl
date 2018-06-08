e50722d0bd6b2878e0009ebf9db97f2b805dc7d9
rofinn <rory.finnegan@gmail.com>
2018-06-08 16:43:00 -0500

    Minimal working DataStreams segfault test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,12 @@
 using DataStreamsTest
+
 @static if VERSION < v"0.7.0-DEV.2005"
     using Base.Test
 else
     using Test
 end
 
-# write your own tests here
-@test 1 == 2
+using DataFrames
+using CSV
+
+tuple.(CSV.read("test.csv").columns...)
